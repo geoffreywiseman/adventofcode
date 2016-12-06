83be28b8f052f7b91b705cb5be45be3eaa322ddd
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2016-12-06 14:30:44 -0500

    Refactored out common Solver code.

--- 2016/day6/PartTwo.java
+++ 2016/day6/PartTwo.java
@@ -3,42 +3,17 @@ import java.nio.file.*;
 import java.util.*;
 import java.util.stream.*;
 
-public class PartTwo {
-	private List<Map<Character,Integer>> columnCharacterFrequency;
+public class PartTwo extends Solver {
 
 	public static void main(String[] arguments) {
 		new PartTwo( "puzzleInput.txt" ).solve();
 	}
 
-	public PartTwo( String filename ) {
-		columnCharacterFrequency = new ArrayList<>();
-		try {
-			Path input = FileSystems.getDefault().getPath( "puzzleInput.txt" );
-			Files.lines( input ).forEach( this::parseLine );
-		} catch( IOException|RuntimeException exception ) {
-			exception.printStackTrace();
-		}
+	public PartTwo( String inputFilename ) {
+		super(inputFilename);
 	}
 
-	private void parseLine( String line ) {
-		if( columnCharacterFrequency.size() == 0 ) {
-			IntStream.range( 0, line.length() )
-				.forEach( i -> columnCharacterFrequency.add( new HashMap<>() ) );
-		} else if( columnCharacterFrequency.size() != line.length() ) {
-			throw new RuntimeException( "columnCharacterFrequency length is " + columnCharacterFrequency.size() +
-				" but string length is " + line.length() );
-		}
-		for( int index = 0; index < line.length(); index++ ) {
-			char letter = line.charAt(index);
-			Map<Character,Integer> map = columnCharacterFrequency.get(index);
-			if( map.containsKey( letter ) )
-				map.put( letter, map.get( letter ) + 1 );
-			else
-				map.put( letter, 1 );				
-		}
-	}
-
-	Character getMostCommonLetter( Map<Character,Integer> frequency ) {
+	protected Character getLetter( Map<Character,Integer> frequency ) {
 		Character letter = frequency.entrySet()
 			.stream()
 			.min( Map.Entry.comparingByValue(Integer::compareTo) )
@@ -47,10 +22,4 @@ public class PartTwo {
 		return letter;
 	}
 
-	public void solve() {
-		System.out.println( "Solving...." );
-		StringBuilder builder = new StringBuilder();
-		columnCharacterFrequency.forEach( (m) -> builder.append( getMostCommonLetter(m) ) );
-		System.out.println( "Most common string: " + builder.toString() );
-	}
 }
