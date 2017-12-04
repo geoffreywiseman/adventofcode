fe7ca7269c65c2c675fd61040d7520dd24c6e837
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2017-12-04 10:45:40 -0500

    Updated Readme, Removed Debug
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

--- 2017/day03/README.md
+++ 2017/day03/README.md
@@ -1,6 +1,12 @@
 # 2017, Day 3
 
-# Part 1
+## Part 1
 
 Well, of course, the first challenge is to be able to figure out the co-ordinates of the Nth element of a spiral without having to lay out each preceding square, since that would be a fairly painful process for a larger number.
+
+I did a little Stack Overflow-ing of the subject, and found some solutions, although none of them truly explained how they were derived, so I went through a somewhat painful process of re-deriving the solution that seemed most legible.  There was a more-efficient version that was less legible, but I couldn't quickly see how it was derived, and I don't like to use code I didn't write and don't completely understand, so ... I left that one.
+
+## Part 2
+
+Phew, this looks like a piece of work on its own. Torn between actually calculating out each value and trying to work out a clever way of deriving it.
 
--- 2017/day03/day03-part1.kts
+++ 2017/day03/day03-part1.kts
@@ -18,37 +18,18 @@ fun getCoordinates(index: Int): Pair<Int, Int> {
 	val ringMax = Math.pow(ringDimension.toDouble(), 2.0).toInt()
 	val rows = getRows(ringMax, ringDimension)
 
-	print("$index:\n\tringDimension:$ringDimension, ringMax:$ringMax, ringIndex:$ringIndex, rows: ${rows.toList()}\n")
-
 	return when (index) {
-		in rows[0] -> {
-			print("\t$index is on bottom row\n")
-			Pair(ringIndex - (ringMax - index), -ringIndex)
-		}
-		in rows[1] -> {
-			print("\t$index is on left row\n")
-			Pair(-ringIndex, ringMax - index - (ringDimension - 1) - ringIndex)
-		}
-		in rows[2] -> {
-			print("\t$index is on top row\n")
-			Pair((ringMax - index - (ringDimension - 1) * 2 - ringIndex), ringIndex)
-		}
-		in rows[3] -> {
-			print("\t$index is on right row\n")
-			Pair(ringIndex, index - (ringMax - (ringDimension - 1) * 4) - ringIndex)
-		}
+		in rows[0] -> Pair(ringIndex - (ringMax - index), -ringIndex)
+		in rows[1] -> Pair(-ringIndex, ringMax - index - (ringDimension - 1) - ringIndex)
+		in rows[2] -> Pair((ringMax - index - (ringDimension - 1) * 2 - ringIndex), ringIndex)
+		in rows[3] -> Pair(ringIndex, index - (ringMax - (ringDimension - 1) * 4) - ringIndex)
 		else -> throw RuntimeException("Can't calulate co-ordinates for index: $index")
 	}
 }
 
-// 1, 2, 3, 4, 5, 6, 7, 8, 9,12,20,23
-// bottom row: 5,6,17,18,19,20
-// left row: 5,6,17,18,19,20
-// top row: 3, 4, 14, 15, 16
-// right row: 2,10,11,12
 arrayOf(361527)
 		.map { getCoordinates(it) }
-		.forEach { (x,y) ->
+		.forEach { (x, y) ->
 			val distance = abs(x) + abs(y);
 			print("distance from ($x,$y): $distance\n")
 		}
