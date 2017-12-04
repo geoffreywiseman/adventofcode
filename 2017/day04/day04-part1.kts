1a1555d42af7de43eb64e237026e003fc0bfd9ba
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2017-12-04 03:13:19 -0500

    Improved Part 1
    I made an improvement in Part 2, so I’m back-porting it to Part 1.
    
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

--- 2017/day04/day04-part1.kts
+++ 2017/day04/day04-part1.kts
@@ -8,15 +8,9 @@ val validCount = valids.count { it }
 print( "There are $validCount valid passphrases." )
 
 fun validPassphrase( passphrase: String ): Boolean {
-	var uniques = mutableListOf<String>()
+	var uniques = mutableSetOf<String>()
 	return passphrase.splitToSequence(" ")
 		.filter { it.length > 0 }
-		.map { word ->
-			if(uniques.contains(word))
-				false
-			else {
-				uniques.add(word)
-				true
-			}
-		}.all { it }
+		.map { word -> uniques.add(word) }
+		.all { it }
 }
