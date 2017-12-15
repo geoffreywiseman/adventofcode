f82a533d9898ada2968b5c4c64de1c3e0041c0e6
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2017-12-15 08:46:20 -0500

    Improved Day 15
    I wanted to turn this into sequences and a more functional approach … so I did.
    
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

--- 2017/day15/day15.kts
+++ 2017/day15/day15.kts
@@ -1,40 +1,28 @@
-class Generator(seed: Int, val factor: Int, val filter: (Long) -> Boolean = { true }) {
-	private var lastValue = seed.toLong()
-	fun next(): Long {
-		var value = lastValue
-		do {
-			value = (value * factor) % 2147483647L
-		} while( !filter(value) )
-		lastValue = value
-		return value
+val mask = 0xffffL
+
+fun generator(seed: Int, factor: Int): Sequence<Long> {
+	return generateSequence(seed.toLong()) { lastValue ->
+		lastValue * factor % Int.MAX_VALUE
 	}
 }
 
-fun duel(generators: Array<Generator>, rounds: Int): Int {
-	var matches = 0
-	repeat(rounds) {
-		val values = generators.map { it.next() }
-				.map { it.toString(2) }
-				.map { it.padStart(32, '0') }
-				.map { it.takeLast(16) }
-		if (values.first() == values.last())
-			matches += 1
-	}
-	return matches
+fun duel(rounds: Int, generators: Pair<Sequence<Long>, Sequence<Long>>): Int {
+	return generators.first
+			.zip(generators.second)
+			.take(rounds)
+			.filter { (first, second) -> (first and mask) == (second and mask) }
+			.count()
 }
 
 // Part 1
-val unfiltered = arrayOf(
-		Generator(618, 16807),
-		Generator(814, 48271)
-)
-val unfilteredMatches = duel(unfiltered, 40_000_000))
+val first = generator(618, 16807)
+val second = generator(814, 48271)
+val unfilteredMatches = duel(40_000_000, Pair(first, second))
 println("Found ${unfilteredMatches} unfiltered matches") // 577
 
 // Part 2
-val filtered = arrayOf(
-		Generator(618, 16807) { it % 4 == 0L },
-		Generator(814, 48271) { it % 8 == 0L }
-)
-val filteredMatches = println(duel(filtered, 5_000_000))
+val filteredMatches = duel(5_000_000, Pair(
+		first.filter { it % 4 == 0L },
+		second.filter { it % 8 == 0L }
+))
 println("Found ${filteredMatches} filtered matches")
