641f1c72f8d76581818130bf5ba5e694685c3929
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2021-12-01 00:44:48 -0500

    Advent of Code 2021 - Day 1
    Day 1 in Kotlin.
    
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

---
+++ 2021/day1/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "aoc-2021-day1"
+
---
+++ 2021/day1/src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    val one = part1(getResourceLines("input.txt"))
+    println("Part 1: depth increased ${one} times")
+
+    val two = part2(getResourceLines("input.txt"))
+    println("Part 2: moving sum depth increased ${two} times")
+}
+
+fun getResourceLines(name: String): Sequence<String> {
+    return Thread.currentThread()
+        .contextClassLoader
+        .getResourceAsStream(name)
+        .bufferedReader()
+        .lineSequence()
+}
+
+fun part1(lines: Sequence<String>): Int {
+    return lines.map(String::toInt)
+        .zipWithNext()
+        .count() {
+            it.second > it.first
+        }
+}
+
+fun part2(lines: Sequence<String>): Int {
+    return lines.map(String::toInt)
+        .windowed(3)
+        .map { it.sum() }
+        .zipWithNext()
+        .count() { it.second > it.first }
+}
